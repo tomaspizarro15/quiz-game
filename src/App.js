5aeecdad01ce30c349ec948362ec9e8c860c93e6
tomaspizarro15 <61084368+tomaspizarro15@users.noreply.github.com>
2021-03-03 11:18:19 -0300

    css Animations finished!

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div className="App_bg">
-          <div className="App_bg_left">
-            <div className="bg_wave"></div>
-          </div>
+      <div className="App_bg">
+        <div className="App_content">
+          <h1 style={{color :'white'}}>MULTI QUIZ GAME</h1>
         </div>
+      </div>
+      <div className="App_bg_left">
+
+        <div className="bg_wave"></div>
+      </div>
     </div>
   );
 }
